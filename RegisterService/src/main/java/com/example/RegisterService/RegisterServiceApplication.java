5a32c1541915af7315726541e0eef530a34293e8
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-08-25 20:35:51 +0530

    config server completed

--- RegisterService/src/main/java/com/example/RegisterService/RegisterServiceApplication.java
+++ RegisterService/src/main/java/com/example/RegisterService/RegisterServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegisterServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterServiceApplication.class, args);
 	}

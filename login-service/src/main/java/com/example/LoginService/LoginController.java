d208d7ff7d2562be8e17b68c691c4a03e3eec435
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-08-29 09:59:31 +0530

    endpoint added to login service for testing

--- login-service/src/main/java/com/example/LoginService/LoginController.java
+++ login-service/src/main/java/com/example/LoginService/LoginController.java
@@ -3,10 +3,7 @@ package com.example.LoginService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 
@@ -36,8 +33,9 @@ public class LoginController {
 
 
     }
-    @GetMapping("/")
+    @GetMapping("/test")
     public String loginWithCredentials() throws UnsupportedEncodingException {
+    return "You are connected";
     }
 
 

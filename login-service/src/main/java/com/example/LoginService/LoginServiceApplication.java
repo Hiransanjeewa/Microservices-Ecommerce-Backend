50554e65c30f00d2695f1f11b396e8cdcf82ae43
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-09-04 18:25:10 +0530

    Eureka server configurations changed , need to solve the error in instance from different component

--- login-service/src/main/java/com/example/LoginService/LoginServiceApplication.java
+++ login-service/src/main/java/com/example/LoginService/LoginServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.LoginService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Component;
 
 
 @SpringBootApplication
+@Component
 public class LoginServiceApplication {
 
 	public static void main(String[] args) {

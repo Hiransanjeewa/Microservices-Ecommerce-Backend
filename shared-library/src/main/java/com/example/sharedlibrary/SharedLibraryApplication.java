69f0b0ae1b7aee84d2a03261b22e09fe6c9fea89
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-09-07 21:48:15 +0530

    Shared libraries added again - trying to use api based library sharing

---
+++ shared-library/src/main/java/com/example/sharedlibrary/SharedLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharedlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedLibraryApplication.class, args);
+	}
+
+}
---
+++ shared-library/src/test/java/com/example/sharedlibrary/SharedLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharedlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
